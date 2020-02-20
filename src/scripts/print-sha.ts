b3e4eb810a3d389d3cd574baad7718c2d82dea08
Harvey Rogers <harvey.rogers@urbanairship.com>
2020-02-20 12:00:36 -0800

    wip

--- src/scripts/print-sha.ts
+++ src/scripts/print-sha.ts
@@ -1,7 +1,8 @@
 import * as github from '../github'
 
 function main(): void {
-  console.log('SHA: %s', github.getSha())
+  console.log('GITHUB_SHA: %s', process.env.GITHUB_SHA)
+  console.log('github.context.sha: %s', github.getSha())
 }
 
 if (require.main === module) {
